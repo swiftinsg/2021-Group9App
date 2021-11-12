a47eb016757ae67072a8aa8ef668f6da8fdaf1bd
Celeste <celestekjx@gmail.com>
2021-11-12 20:41:04 +0800

    Initial Commit

---
+++ Group 9 App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Group 9 App
+//
+//  Created by Celeste on 12/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Group 9 App/Group_9_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Group_9_AppApp.swift
+//  Group 9 App
+//
+//  Created by Celeste on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Group_9_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
