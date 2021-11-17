e2bc6e4e4fa11dc5b49c34cad531efa0c0f7dd9d
uncoolbeans <ivcleandedmyroom@gmail.com>
2021-11-17 11:24:04 +0800

    Linked the code to a quote API that dispense quotes

---
+++ Group 9 App/QuoteManager.swift
@@ -0,0 +1,30 @@
+//
+//  QuoteManager.swift
+//  Group 9 App
+//
+//  Created by MMS on 17/11/21.
+//
+
+import Foundation
+import SwiftUI
+
+class QuoteManager: ObservableObject{
+    
+    @Published var quote: Quote?
+    
+    func getQuote(){
+        let apiURL = URL(string: "https://type.fit/api/quotes")!
+        let request = URLRequest(url: apiURL)
+        
+        quote = nil
+        
+        URLSession.shared.dataTask(with: request){ data, response, error in
+            if let data = data {
+                let decoder = JSONDecoder()
+                DispatchQueue.main.async {
+                    self.quote = try? decoder.decode(Quote.self, from: data)
+                }
+            }
+        }.resume()
+    }
+}
