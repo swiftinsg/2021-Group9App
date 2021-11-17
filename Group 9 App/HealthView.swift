c0aaed470010fab443a6b3e25ba993f390a95bae
Celeste <celestekjx@gmail.com>
2021-11-17 13:43:13 +0800

    sheet fundamentals

--- Group 9 App/HealthView.swift
+++ Group 9 App/HealthView.swift
@@ -3,18 +3,23 @@ import SwiftUICharts
 
 struct HealthView: View {
     var stressData: [Double] = [8, 2, 4, 6, 12, 9, 2]
-    @State var showAddSheet = false
+    @State var showFormSheet = false
     @State var showQuoteSheet = false
+    @State var showAdviceSheet = false
+    @State var showTipsSheet = false
     var body: some View {
         NavigationView{
             List{
                 Button("Health Form"){
-                    showAddSheet = true
+                    showFormSheet = true
                 }
                 Button("Quote of the Day"){
                     showQuoteSheet = true
                 }
                 Button("Advice of the Day"){
+                    
+                }
+                Button("Study Tips"){
                     
                 }
                 LineChart()
