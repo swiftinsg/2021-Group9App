d77dc07a2de5af8dfd1bb0db64ad34304026c085
Celeste <celestekjx@gmail.com>
2021-11-17 10:07:52 +0800

    Haven't rlly done anything here but will push first

--- Group 9 App/HealthView.swift
+++ Group 9 App/HealthView.swift
@@ -1,15 +1,12 @@
-//
-//  HealthView.swift
-//  Group 9 App
-//
-//  Created by Celeste on 17/11/21.
-//
-
 import SwiftUI
 
 struct HealthView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            List{
+                
+            }
+        }
     }
 }
 
